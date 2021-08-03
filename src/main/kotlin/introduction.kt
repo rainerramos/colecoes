b8661333b1db19df89a5d1b6debc417a60ad1c5a
Rainer Ramos <rainermr89@hotmail.com>
2021-08-03 17:38:08 -0300

    Usando Coleções - Parte 2

--- src/main/kotlin/introduction.kt
+++ src/main/kotlin/introduction.kt
@@ -4,6 +4,11 @@ data class Receita(val nome: String, val calorias: Int, val ingredientes: List<I
 
 data class Ingredientes(val nome: String, val quantidade: Int)
 
+fun hasIngredient(list: List<Ingredientes>, nome: String): Boolean {
+    return list.filter { it.nome == nome }.any()
+
+}
+
 
 fun main(args: Array<String>) {
 
@@ -52,11 +57,27 @@ fun main(args: Array<String>) {
     // listOf(1,2,3,4,5,3,2).sum()
 
     // Me dê as duas primeiras receitas
-    val firstTwo = data. take(2)
+    val firstTwo = data.take(2)
     for (x in firstTwo.withIndex()) {
         println("${x.index + 1} - ${x.value.nome}") // o +1 é apenas para começar do 1 e não do 0
         //o withIndex tudo o que ele fez foi transformar essa lista num tipo de dado que tem um index e um valor para esse index
     }
 
+    // Sei como fazer panqueca? E sushi?
+    val knowPancake = data.filter { it.nome == "Panqueca" }.any()
+    println("Sei fazer panqueca? ${if (knowPancake) "sim" else "não"}")
+
+    val knowSushi = data.filter { it.nome == "Sushi" }.any()
+    println("Sei fazer panqueca? ${if (knowSushi) "sim" else "não"}")
+
+    // Quais são as comidas com mais de 500 calorias?
+    val more500 = data.filter { it.calorias > 500 }.forEach { println(it.nome) }
+
+    // Par (chave, valor) de comidas com mais de 500 calorias (nome, calorias)
+    data.filter { it.calorias > 500 }.map { Pair(it.nome, it.calorias) }
+        .forEach { println("${it.first}: ${it.second} calorias.") }
+
+    // Quais das receitas possui farinha como ingrediente?
+    val result = data.filter { hasIngredient(it.ingredientes, "Farinha") }.forEach { println(it.nome) }
 
 }
