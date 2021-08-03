81a3c642575acd38e2083d65f4a94201f2422ad8
Rainer Ramos <rainermr89@hotmail.com>
2021-08-03 17:10:38 -0300

    Usando Coleções - Parte 1

--- src/main/kotlin/introduction.kt
+++ src/main/kotlin/introduction.kt
@@ -1,19 +1,62 @@
 package collections
 
+data class Receita(val nome: String, val calorias: Int, val ingredientes: List<Ingredientes> = listOf())
+
+data class Ingredientes(val nome: String, val quantidade: Int)
+
+
 fun main(args: Array<String>) {
 
-        // List - array, Set, Hashmap
+    // Cria lista de dados
+    val data = listOf(
+        Receita(
+            "Lasanha", 1200,
+            listOf(
+                Ingredientes("Farinha", 1),
+                Ingredientes("Presunto", 5),
+                Ingredientes("Queijo", 10),
+                Ingredientes("Molho de tomate", 2),
+                Ingredientes("Manjerição", 3)
+            )
+        ),
+        Receita("Panqueca", 500),
+        Receita("Omelete", 200),
+        Receita("Parmegiana", 700),
+        Receita("Sopa de feijão", 300),
+        Receita(
+            "Hamburguer", 2000,
+            listOf(
+                Ingredientes("Pão", 1),
+                Ingredientes("Hamburguer", 3),
+                Ingredientes("Queijo", 1),
+                Ingredientes("Catupiry", 1),
+                Ingredientes("Bacon", 3),
+                Ingredientes("Alface", 1),
+                Ingredientes("Tomate", 1)
+            )
+        )
+    )
 
-    val l1 = listOf("Madrid", "São Paulo", "Berlim") // a listOf é IMUTÁVEL
-    val l2 = mutableListOf("Madrid", "São Paulo", "Berlim")
-    val a1 = arrayListOf("Madrid", "São Paulo", "Berlim")
+    // Tenho receitas na lista?
+    println("tenho receitas? ${if (data.any()) "sim" else "não"}.")
 
-    val s1 = setOf("Madrid", "São Paulo", "Berlim", "Berlim") //o setOf impede que elementos repetidos sejam inseridos
-    val s2 = mutableSetOf("Madrid", "São Paulo", "Berlim", "Berlim")
+    // Quantas receitas tenho na coleção?
+    println("Tenho ${data.count()} receitas.")
 
-    val h1 = hashMapOf<String, String>(Pair("key", "value"), Pair("França", "Paris"))
-    println(h1["França"]) // a partir de uma chave eu coonsigo ter um valor e tb hashMapOf já é mutável
+    // Qual a primeira e última receita?
+    println("A primeira receita é: ${data.first().nome}")
+    println("A última receita é: ${data.last().nome}")
+    // Qual a soma de calorias?
+    val sumCalories = data.sumOf { it.calorias }
+    println("A soma de calorias é: $sumCalories")
+    // listOf(1,2,3,4,5,3,2).sum()
 
-    val n1 = mapOf(Pair("key", "value"), Pair("França", "Paris")) //aqui eu não tenho o "add"
-    val n2 = mutableMapOf(Pair("key", "value"), Pair("França", "Paris")) // aqui eu tneho o entries.add
+    // Me dê as duas primeiras receitas
+    val firstTwo = data. take(2)
+    for (x in firstTwo.withIndex()) {
+        println("${x.index + 1} - ${x.value.nome}") // o +1 é apenas para começar do 1 e não do 0
+        //o withIndex tudo o que ele fez foi transformar essa lista num tipo de dado que tem um index e um valor para esse index
+    }
+
+
 }
